e745dffd956fa0f1043d2f115df3fdc88f46d94b
mr_wj <1477563131@qq.com>
2018-12-24 10:23:13 +0800

    update 3

--- src/main/java/com/collect/cn/CollectLog.java
+++ src/main/java/com/collect/cn/CollectLog.java
@@ -8,7 +8,7 @@ public class CollectLog {
     public static void main(String[] args) {
         Timer timer = new Timer();
 
-
+        //定时器任务
         timer.schedule( new CollectTask(),0,60*60*1000L);
 
     }
