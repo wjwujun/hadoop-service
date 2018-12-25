5e2cffea89f4519e7cb1af3d7784263e4014eb19
mr_wj <1477563131@qq.com>
2018-12-25 14:01:19 +0800

    定时清除文件功能

--- src/main/java/com/collect/cn/BackCleanTask.java
+++ src/main/java/com/collect/cn/BackCleanTask.java
@@ -1,10 +1,42 @@
 package com.collect.cn;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.TimerTask;
 
 public class BackCleanTask extends TimerTask {
     @Override
     public void run() {
+        //探测备份目录
+        File backDir = new File("E:/javaproject/logs/back/");
+        File[] listDir = backDir.listFiles();
+
+        //判断备份日期子目录是否已经超过24小时。
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH");
+        long now = new Date().getTime();
+
+
+
+        for (File file : listDir) {
+            try {
+                long time = sdf.parse(file.getName()).getTime();
+                if(now - time>24*60*60*1000L){
+
+                    FileUtils.deleteDirectory(file);
+
+                }
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
 
     }
 }
