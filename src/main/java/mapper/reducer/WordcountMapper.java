eaba499f3407776a779060f58541c0d6a164c4d7
mr_wj <1477563131@qq.com>
2018-12-27 11:05:55 +0800

    windows系统中运行job客户端提交到yarn上运行

---
+++ src/main/java/mapper/reducer/WordcountMapper.java
@@ -0,0 +1,25 @@
+package mapper.reducer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordcountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        // 切单词
+        String line = value.toString();
+        String[] words = line.split(" ");
+        for(String word:words){
+            context.write(new Text(word), new IntWritable(1));
+
+        }
+    }
+}
+
